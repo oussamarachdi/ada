14d7d47dcc770d2658f857130fbb0fffc10ca198
oussamarachdi <rachdioussama33@gmail.com>
2020-09-08 01:11:15 +0100

    simple changes in dashboard admin

--- ada/ada_app/admin.py
+++ ada/ada_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+#from django.contrib.auth.models import Group
 from ada_app.model import (Product,
                            ProductImage,
                            SubCategory,
@@ -8,9 +9,16 @@ from ada_app.model import (Product,
                            MyWishList,
                            MyProduct)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creationdate')
+    list_filter = ('creationdate',)
+
+
+admin.site.site_header = 'ADA ADMIN'
+#admin.site.unregister(Group)
 admin.site.register(Profile)
 admin.site.register(ProductImage)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(SubCategory)
 admin.site.register(Category)
 admin.site.register(MyWishList)

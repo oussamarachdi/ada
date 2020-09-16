32a544529eb8843ce05d5fc2f96a6c760adb0a54
oussamarachdi <rachdioussama33@gmail.com>
2020-09-16 13:39:07 +0100

    dashboard2

--- ada/ada_app/admin.py
+++ ada/ada_app/admin.py
@@ -10,7 +10,7 @@ from ada_app.model import (Product,
                            MyProduct)
 
 class ProductAdmin(admin.ModelAdmin):
-        list_display = ('name', 'creationdate')
+        list_display = ('name', 'creationdate','category','subcategory')
         list_filter = ('creationdate',)
         search_fields = ('name__exact',)
 
@@ -20,15 +20,77 @@ class ProductAdmin(admin.ModelAdmin):
 
             return  ('name',)
 
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = ('name','numberOfSearchRequests')
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name', 'numberOfSearchRequests')
+            return ('name',)
+
+class ProfileAdmin(admin.ModelAdmin):
+        list_display = ('name','user', 'email')
+        list_filter = ('id',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name','user', 'email')
+            return ('name',)
+
+class ProductImageAdmin(admin.ModelAdmin):
+        list_display = ('name','product')
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name','product',)
+            return ('name',)
+class SubCategoryAdmin(admin.ModelAdmin):
+        list_display = ('name','category')
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name','category',)
+            return ('name',)
+class MyWishListAdmin(admin.ModelAdmin):
+        list_display = ('name','user')
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name','user',)
+            return ('name',)
+class MyProductAdmin(admin.ModelAdmin):
+        list_display = ('name','myproduct')
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
+        def get_ordering(self, request):
+            if request.user.is_superuser:
+                return ('name','myproduct',)
+            return ('name',)
+
+class RegionAdmin(admin.ModelAdmin):
+        list_display = ('name',)
+        list_filter = ('name',)
+        search_fields = ('name__exact',)
+
 admin.site.site_header = 'Ada Admin'
 admin.site.site_title = 'Ada Admin'
 admin.site.index_title = 'Ada Administration'
 #admin.site.unregister(Group)
-admin.site.register(Profile)
-admin.site.register(ProductImage)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(SubCategory)
-admin.site.register(Category)
-admin.site.register(MyWishList)
-admin.site.register(MyProduct)
-admin.site.register(Region)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(MyWishList, MyWishListAdmin)
+admin.site.register(MyProduct, MyProductAdmin)
+admin.site.register(Region, RegionAdmin)
